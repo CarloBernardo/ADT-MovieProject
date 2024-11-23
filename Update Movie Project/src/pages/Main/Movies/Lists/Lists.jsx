6d4d53f12e255db39ebadcf31fe52861cf34d835
CarloBernardo <Carlobernardo778@gmail.com>
2024-11-23 08:55:24 +0800

    Add files via upload

--- Update Movie Project/src/pages/Main/Movies/Lists/Lists.jsx
+++ Update Movie Project/src/pages/Main/Movies/Lists/Lists.jsx
@@ -3,20 +3,51 @@ import './Lists.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 const Lists = () => {
+  const accessToken = localStorage.getItem('accessToken');
   const navigate = useNavigate();
   const [lists, setLists] = useState([]);
 
-  useEffect(() => {
+  const getMovies = () => {
+    //get the movies from the api or database
     axios.get('/movies').then((response) => {
       setLists(response.data);
     });
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
 
-  
+  const handleDelete = (id) => {
+    const isConfirm = window.confirm(
+      'Are you sure that you want to delete this data?'
+    );
+    if (isConfirm) {
+      axios
+        .delete(`/movies/${id}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then(() => {
+          //update list by modifying the movie list array
+          const tempLists = [...lists];
+          const index = lists.findIndex((movie) => movie.id === id);
+          if (index !== undefined || index !== -1) {
+            tempLists.splice(index, 1);
+            setLists(tempLists);
+          }
+
+          //update list by requesting again to api
+          // getMovies();
+        });
+    }
+  };
+
   return (
     <div className='lists-container'>
       <div className='create-container'>
         <button
+        className="button-list"
           type='button'
           onClick={() => {
             navigate('/main/movies/form');
@@ -31,24 +62,32 @@ const Lists = () => {
             <tr>
               <th>ID</th>
               <th>Title</th>
+              <th>Popularity</th>
+              <th>Release Date</th>
               <th>Actions</th>
+              
             </tr>
           </thead>
           <tbody>
-            {lists.map((movie, index) => (
+            {lists.map((movie) => (
               <tr>
-                <td>{index}</td>
+                <td>{movie.id}</td>
                 <td>{movie.title}</td>
+                <td>{movie.popularity}</td>
+                <td>{movie.dateCreated}</td>
                 <td>
                   <button
+                  className="button-list"
                     type='button'
                     onClick={() => {
-                      navigate('/main/movies/form/' + index);
+                      navigate('/main/movies/form/' + movie.id);
                     }}
                   >
                     Edit
                   </button>
-                  <button type='button'>Delete</button>
+                  <button className="button-list" type='button' onClick={() => handleDelete(movie.id)}>
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
